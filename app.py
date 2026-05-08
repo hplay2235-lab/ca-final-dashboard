f132cf8ed24e8f9fc0a6d47d803beb119ae12247
hplay2235-lab <hplay2235@gmail.com>
2026-05-09 01:50:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import streamlit as st import pandas as pd import plotly.express as px from datetime import datetime
 
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from datetime import datetime
 ---------------------------
 
 PAGE CONFIG
